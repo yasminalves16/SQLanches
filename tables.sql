975747b09c67c5f7d916033bda6a28c23f6b3e8d
Yasmin Alves <95253622+yasminalves16@users.noreply.github.com>
2022-06-20 01:27:54 -0300

    fix: foreign key

--- tables.sql
+++ tables.sql
@@ -1,19 +1,16 @@
--- Aqui você deve colocar os códigos SQL referentes à
+-- Criação das tabelas:
 
--- Criação das tabelas
-
+-- Tabela clientes
 CREATE TABLE IF NOT EXISTS clientes(
   id BIGSERIAL PRIMARY KEY,
   nome VARCHAR(50) NOT NULL,
   lealdade INTEGER NOT NULL
-
 );
 
 -- Tabela endereços
-
-CREATE TABLE IF NOT EXISTS endereços(
+CREATE TABLE IF NOT EXISTS enderecos(
   id BIGSERIAL PRIMARY KEY,
-  cep VARCHAR(50) NOT NULL,
+  cep VARCHAR(9) NOT NULL,
   rua VARCHAR(50) NOT NULL,
   numero INTEGER NOT NULL,
   bairro VARCHAR(50) NOT NULL,
@@ -23,30 +20,28 @@ CREATE TABLE IF NOT EXISTS endereços(
 );
 
 -- Tabela pedidos
-
 CREATE TABLE IF NOT EXISTS pedidos(
   id BIGSERIAL PRIMARY KEY,
   status VARCHAR(50) NOT NULL,
   cliente_id INTEGER NOT NULL,
-  FOREIGN KEY (cliente_id) REFERENCES clientes(id) ON DELETE CASCADE
+  FOREIGN KEY (cliente_id) REFERENCES clientes(id) ON DELETE CASCADE 
 );
 
 -- Tabela produtos
-
 CREATE TABLE IF NOT EXISTS produtos(
   id BIGSERIAL PRIMARY KEY,
   nome VARCHAR(100) UNIQUE NOT NULL,
   tipo VARCHAR(30) NOT NULL,
-  preço FLOAT NOT NULL,
+  preco FLOAT NOT NULL,
   pts_de_lealdade INTEGER NOT NULL
 );
 
 -- Tabela produtos_pedidos
-
 CREATE TABLE IF NOT EXISTS produtos_pedidos(
   id BIGSERIAL PRIMARY KEY,
   pedido_id INTEGER NOT NULL,
-  FOREIGN KEY (pedido_id) REFERENCES pedidos(id) ON DELETE CASCADE,
   produto_id INTEGER NOT NULL,
-  FOREIGN KEY (produto_id) REFERENCES produtos(id)
+  FOREIGN KEY (pedido_id) REFERENCES pedidos(id) ON DELETE CASCADE,
+  FOREIGN KEY (produto_id) REFERENCES produtos(id) ON DELETE CASCADE
 );
+
