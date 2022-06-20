389dc6911072f5133939a81cf9c133c417d7137b
Yasmin Alves <95253622+yasminalves16@users.noreply.github.com>
2022-06-20 01:28:40 -0300

    Update selects.sql

--- selects.sql
+++ selects.sql
@@ -1,62 +1,50 @@
--- Aqui você deve colocar os códigos SQL referentes às
+--- Aqui você deve colocar os códigos SQL referentes às
 -- Seleções de dados
 
 -- 1)
-
-SELECT
-  ped.id,
-  ped.status,
-  ped.cliente_id,
+SELECT 
+  p.id,
+  p.status,
+  p.cliente_id,
   prod.id,
   prod.nome,
   prod.tipo,
-  prod.preço,
+  prod.preco,
   prod.pts_de_lealdade
-FROM
-  produtos_pedidos AS pp
-  INNER JOIN pedidos ped ON ped.id = pp.pedido_id
-  INNER JOIN produtos prod ON prod.id = pp.produto_id;
+FROM pedidos p
+INNER JOIN produtos_pedidos prodp ON p.id = prodp.pedido_id
+INNER JOIN produtos prod ON prodp.produto_id = prod.id;
 
 -- 2)
-
-SELECT
-  ped.id
-FROM
-  produtos_pedidos AS pp
-  INNER JOIN pedidos ped ON ped.id = pp.pedido_id
-  INNER JOIN produtos prod ON prod.id = pp.produto_id
-  WHERE prod.nome LIKE 'Fritas';
+SELECT 
+ p.id
+FROM pedidos p
+INNER JOIN produtos_pedidos prodp ON p.id = prodp.pedido_id
+INNER JOIN produtos prod ON prodp.produto_id = prod.id
+WHERE prod.nome ILIKE '%fritas';
 
 -- 3)
-
 SELECT
-  c.nome as gostam_de_fritas
-FROM
-  produtos_pedidos AS pp
-  INNER JOIN pedidos ped ON ped.id = pp.pedido_id
-  INNER JOIN produtos prod ON prod.id = pp.produto_id
-  INNER JOIN clientes c ON c.id = ped.cliente_id 
-  WHERE prod.nome LIKE 'Fritas';
+  c.nome AS gostam_de_fritas
+FROM clientes c
+INNER JOIN pedidos p ON c.id = p.cliente_id
+INNER JOIN produtos_pedidos prodp ON p.id = prodp.pedido_id
+INNER JOIN produtos prod ON prodp.produto_id = prod.id
+WHERE prod.nome ILIKE '%fritas';
 
 -- 4)
-
-SELECT
-  SUM(preço)
-FROM
-  produtos_pedidos AS pp
-  INNER JOIN pedidos ped ON ped.id = pp.pedido_id
-  INNER JOIN produtos prod ON prod.id = pp.produto_id
-  INNER JOIN clientes c ON c.id = ped.cliente_id 
-  WHERE c.nome LIKE 'Laura';
+SELECT 
+  SUM(prod.preco)
+FROM produtos prod
+INNER JOIN produtos_pedidos prodp ON prod.id = prodp.produto_id
+INNER JOIN pedidos p ON p.id = prodp.pedido_id
+INNER JOIN clientes c ON c.id = p.cliente_id
+WHERE p.cliente_id = 5;
 
 -- 5)
-
 SELECT
-  prod.nome, COUNT(*)
-FROM
-  produtos_pedidos AS pp
-  INNER JOIN pedidos ped ON ped.id = pp.pedido_id
-  INNER JOIN produtos prod ON prod.id = pp.produto_id
-  INNER JOIN clientes c ON c.id = ped.cliente_id
-  GROUP BY prod.nome
-  ORDER BY prod.nome;
+  prod.nome, 
+COUNT(prod.nome)
+FROM produtos prod
+INNER JOIN produtos_pedidos prodp ON prod.id = prodp.produto_id
+GROUP BY prod.nome ORDER BY prod.nome ASC;
